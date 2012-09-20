6a21eaf95bd64bfb2b28771d969faf27c6ba0da1
James Turner <jturnergit@gmail.com>
2012-09-20 18:58:31 +0100

    Initial Creation

---
+++ lib.js
@@ -0,0 +1,25 @@
+(function() {
+
+	window.onload = function() {
+		function sel(query) {
+			var selector = query.substr(1, 1);
+
+	/*
+			//isolate first char 
+			if ( selector === '#' ) {
+				//id
+			} 
+			else if ( selector === '.' ) {
+
+			}
+	*/
+		}
+		
+	}
+
+
+
+//support for stacking i.e. div p .class #id * {
+	//put in to array separating at space, for loop to loop through all and narrow down selection
+
+})();
